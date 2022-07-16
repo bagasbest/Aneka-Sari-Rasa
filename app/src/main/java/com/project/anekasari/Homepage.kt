418e8736cd7674da141b523f7a6603a27ebce876
Bagas Pangestu <bagas.p@elabram.com>
2022-07-16 07:24:46 +0700

    menghilangkan menu keranjang pada admin

--- app/src/main/java/com/project/anekasari/Homepage.kt
+++ app/src/main/java/com/project/anekasari/Homepage.kt
@@ -1,12 +1,16 @@
 package com.project.anekasari
 
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.project.anekasari.databinding.ActivityHomepageBinding
 
 class Homepage : AppCompatActivity() {
@@ -19,11 +23,42 @@ class Homepage : AppCompatActivity() {
         binding = ActivityHomepageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        checkRole()
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_homepage)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        navView.setupWithNavController(navController)
+    private fun checkRole() {
+        val progressDialog = ProgressDialog(this)
+        progressDialog.setMessage("Silahkan tunggu hingga proses selesai...")
+        progressDialog.setCancelable(false)
+        progressDialog.show()
+
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+        FirebaseFirestore
+            .getInstance()
+            .collection("users")
+            .document(uid)
+            .get()
+            .addOnSuccessListener {
+                val role = "" + it.data!!["role"]
+
+                if(role == "user") {
+                    binding.clUser.visibility =  View.VISIBLE
+                    val navView: BottomNavigationView = binding.navView
+
+                    val navController = findNavController(R.id.nav_host_fragment_activity_homepage)
+                    // Passing each menu ID as a set of Ids because each
+                    // menu should be considered as top level destinations.
+                    navView.setupWithNavController(navController)
+                } else {
+                    binding.clAdmin.visibility =  View.VISIBLE
+                    val navView2: BottomNavigationView = binding.navView2
+
+                    val navController2 = findNavController(R.id.nav_host_fragment_activity_homepage2)
+                    // Passing each menu ID as a set of Ids because each
+                    // menu should be considered as top level destinations.
+                    navView2.setupWithNavController(navController2)
+                }
+                progressDialog.dismiss()
+            }
     }
 }
